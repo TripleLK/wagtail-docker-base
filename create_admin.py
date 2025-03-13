3f1989380db9679ecbe4d0ff9bc2b8cff4024f9e
Lucy Patton <lucy.patton.368@gmail.com>
2025-03-13 14:13:32 -0400

    Basic wagtail site functionality

---
+++ create_admin.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+import os
+import django
+
+# Set the default settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lllk-wagtail-base.settings.dev")
+django.setup()
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "defaultpassword")
