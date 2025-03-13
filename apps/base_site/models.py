d0eb7dbf8630183b1b431cd37e75244226f6a1da
Lucy Patton <lucy.patton.368@gmail.com>
2025-03-13 15:10:30 -0400

    Added basic page model and template to base site

---
+++ apps/base_site/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from wagtail.models import Page
+
+
+class BasicPage(Page):
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + ["intro", "body"]
