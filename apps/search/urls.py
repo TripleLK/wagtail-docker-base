5ed73a5a1eadb0c9f2e58ac1b3b27e93bffd05e0
lucyP368 <141864680+lucyP368@users.noreply.github.com>
2025-05-16 11:36:01 -0400

    Revert "Scrapers expanded"

--- apps/search/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.search import views
-
-urlpatterns = [
-    path("search/", views.search, name="search"),
-    path("category/<str:category_slug>/<str:value_slug>/", views.category_view, name="category_view"),
-] 
